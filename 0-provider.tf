2f6a0348215127f633c0993bcd1ee5bb7f233dc4
sunny <49833687+sunnyyangyyc@users.noreply.github.com>
2022-09-19 21:30:09 +1000

    Update 0-provider.tf

--- 0-provider.tf
+++ 0-provider.tf
@@ -10,3 +10,12 @@ terraform {
 provider "aws" {
  
 }
+
+  backend "s3" {
+    key            = "${var.statefilepath}"     #"uat/frontend/terraform.tfstate"
+    region         = "ap-southeast-2"
+    bucket         = "cc-terraform-state-file"
+    dynamodb_table = "terraform-state-locking"
+    #encrypt = true # Optional, S3 Bucket Server Side Encryption
+  }
+}
